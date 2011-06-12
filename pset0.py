542cdfd33344fffcfcb33cba2a8276e83a9187d2
Will Parker <william2002uk@hotmail.com>
2011-06-12 11:14:12 +0100

    read and write user input

---
+++ pset0.py
@@ -0,0 +1,7 @@
+#Problem set 0
+#Name: Will Parker
+#Collaborators: n/a
+#Time: 00:00:2
+surname = input('Please enter your surname:')
+firstname = input('Please enter your firstname:')
+print('Hello ' + firstname + ' ' + surname)
